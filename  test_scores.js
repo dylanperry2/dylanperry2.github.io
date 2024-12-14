e4b67fa462144226560ed01a12b0e32f73ecd1f8
dylanperry2 <dylanperry101@gmail.com>
2024-12-13 23:03:58 -0600

    g

---  test_scores.js
+++  test_scores.js
@@ -10,4 +10,4 @@ const html1 = `<p>Score 1 = ${score1}</p>
 <p>Score 2 = ${score2}</p>
 <p>Score 3 = ${score3}</p>
 <p>Average score = ${average}</p>`;
-document.write(html1);
+document.write(jsdemo.html);
